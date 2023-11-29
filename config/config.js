6f9d82a07e589a529e0496a4a154b3cb5351a398
Muhammad Ravi <muhammadravi1127@gmail.com>
2023-11-29 09:38:57 +0700

    get Production

--- config/config.js
+++ config/config.js
@@ -21,6 +21,7 @@ const config = {
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST_PROD,
     dialect: process.env.DB_DIALECT_PROD,
+    port: process.env.DB_PORT_PROD,
   },
 };
 
